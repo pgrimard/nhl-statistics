4a5929fa23f2309dd99603482e1ddb0ac279c7cb
Patrick Grimard <pgrimard@gmail.com>
2016-03-12 09:44:39 -0500

    Initial commit.

---
+++ src/main/java/com/patrickgrimard/nhl/NhlStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.patrickgrimard.nhl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhlStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhlStatisticsApplication.class, args);
+	}
+}
