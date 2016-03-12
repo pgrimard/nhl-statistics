b25bf9c8c16bdc36efd7ed99c988e12e2df28c4e
Patrick Grimard <pgrimard@gmail.com>
2016-03-12 12:29:21 -0500

    Enabled discovery client and added RestTemplate bean.

--- src/main/java/com/patrickgrimard/nhl/NhlStatisticsApplication.java
+++ src/main/java/com/patrickgrimard/nhl/NhlStatisticsApplication.java
@@ -2,11 +2,20 @@ package com.patrickgrimard.nhl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NhlStatisticsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NhlStatisticsApplication.class, args);
 	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
